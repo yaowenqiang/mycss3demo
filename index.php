7c764ebafa208c75e71b23532c6f39ee0dfd5d07
yaowenqiang <yaowenqiang111@163.com>
2011-08-28 18:22:30 +0800

    seperate js.

--- index.php
+++ index.php
@@ -3,19 +3,11 @@
 <head>
 	<meta charset="UTF-8">
     <title>css3 selector demo</title>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-<!--[if (gte IE 6)&(lte IE 8)]>
-  <script  src="js/selectivizr.js"></script>
-<![endif]-->
-    <link rel="stylesheet" href="css/mycss.css" />
 </head>
 <body>
 <div class="wrapper">
-    <div>div1</div>
-    <div>div 2</div>
-    <div>div 3</div>
-    <div id="demo">div 3</div>
-    <div id="demo divs">div 3</div>
+<a href="jquery.html">jquery</a>
+<a href="mootools.html">mootools</a>
 </div>
 </body>
 </html>
