f93876ba43878170fc59a5bd38bf762cace50454
yaowenqiang <yaowenqiang111@163.com>
2011-08-28 17:25:47 +0800

    move out the stylesheet.

--- index.php
+++ index.php
@@ -4,14 +4,10 @@
 	<meta charset="UTF-8">
     <title>css3 selector demo</title>
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
-    <script src="js/selectivizr.js"></script>
-    <style type="text/css">
-    div.wrapper{width:400px;height:400px;margin:0 auto;}
-   div.wrapper div{height:100px;} 
-   div.wrapper div:nth-child(2){color:red;} 
-   div.wrapper div:nth-child(2+1){color:orange;} 
-   div[id]{color:yellow;} 
-    </style>
+<!--[if (gte IE 6)&(lte IE 8)]>
+  <script  src="js/selectivizr.js"></script>
+<![endif]-->
+    <link rel="stylesheet" href="css/mycss.css" />
 </head>
 <body>
 <div class="wrapper">
