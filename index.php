cb67fe1b55e4c1f578529336d32d4a7f16901885
yaowenqiang <yaowenqiang111@163.com>
2011-08-28 11:25:35 +0800

    css selector test.

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
-<?php 
-echo "hello world!";
-//phpinfo();
-$hostname = "10.194.111.8";
-$username = "user_90e0c52c";
-$password = "SVc_T1B1IeY!Tk";
-$dbname = "db_90e0c52c";
-mysql_connect() or die(mysql_error());
-?>
-
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>css3 selector demo</title>
+    <script src="js/selectivizr.js"></script>
+    <style type="text/css">
+    div.wrapper{width:400px;height:400px;margin:0 auto;}
+   div.wrapper div{height:100px;} 
+   div.wrapper div:even{color:red;} 
+   div.wrapper div:odd{color:orange;} 
+    </style>
+</head>
+<body>
+<div class="wrapper">
+    <div>div1</div>
+    <div>div 2</div>
+    <div>div 3</div>
+</div>
+	
+</body>
+</html>
