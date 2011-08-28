353f625227d3125835a0ed570994a08654ccd725
yaowenqiang <yaowenqiang111@163.com>
2011-08-28 11:00:41 +0800

    change connection

--- index.php
+++ index.php
@@ -5,6 +5,6 @@ $hostname = "10.194.111.8";
 $username = "user_90e0c52c";
 $password = "SVc_T1B1IeY!Tk";
 $dbname = "db_90e0c52c";
-$mysql_connect() or die(mysql_error());
+mysql_connect() or die(mysql_error());
 ?>
 
