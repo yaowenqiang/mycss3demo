bc9976fcd123e542b939bb468db58f83b9ad7122
yaowenqiang <yaowenqiang111@163.com>
2011-08-28 10:57:35 +0800

    connect to db.

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php 
 echo "hello world!";
-phpinfo();
+//phpinfo();
+$hostname = "10.194.111.8";
+$username = "user_90e0c52c";
+$password = "SVc_T1B1IeY!Tk";
+$dbname = "db_90e0c52c";
+$mysql_connect() or die(mysql_error());
 ?>
 
