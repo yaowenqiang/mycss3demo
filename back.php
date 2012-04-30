640c10251a5c689b46e46d53fa3c4f6175317446
yaowenqiang <yaowenqiang111@163.com>
2012-04-30 12:00:22 +0800

    add callback response

--- back.php
+++ back.php
@@ -1,3 +1,18 @@
 <?php
-    phpinfo();
+if (isset( $_GET['code']) && !empty($_GET['state'])) {
+$remoteUrl = 'https://graph.qq.com/oauth2.0/token';		//远程请求地址 
+$params = array(	//构造请求参数
+	'grant_type' => 'code',
+	'client_id' => '100266584',
+	'client_secret' =>'2e499a2c58e20f1adb74c31d497a96b4',
+	'code' => $_GET['code'],
+	'state' => $_GET['state'],
+	'redirect_uri' => 'http://mycss3demo.orchestra.io/back.php'
+);
+	$requestUrl = $remoteUrl.http_build_query($params);
+	// header('location:'$remoteUrl);
+	redirect($remoteUrl);
+}else {
+	var_dump($_GET);
+}
 ?>
