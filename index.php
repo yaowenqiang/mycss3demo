73ceeff03fb17d03f4f38d96f87d8279c25657c5
yaowenqiang <yaowenqiang111@163.com>
2012-04-30 09:42:50 +0800

    add qq login button

--- index.php
+++ index.php
@@ -1,14 +1,31 @@
 <!DOCTYPE HTML>
 <html lang="en">
 <meta property="qc:admins" content="341076627333345576723053421617" />
+<script type="text/javascript"
+src="http://qzonestyle.gtimg.cn/qzone/openapi/qc.js#appId=APPID" charset="utf-8"></script> 
 <head>
 	<meta charset="UTF-8">
-    <title>css3 selector demo</title>
+	<?php
+    // <title>css3 selector demo</title>
+	?>
+	<title>qq 登陆</title>
 </head>
 <body>
 <div class="wrapper">
-<a href="jquery.html">jquery</a>
-<a href="mootools.html">mootools</a>
+	<?php
+// <a href="jquery.html">jquery</a>
+// <a href="mootools.html">mootools</a>
+	?>
+
+<span id="qqLoginBtn"></span>
+<script type="text/javascript">
+    QC.Login({
+       btnId:"qqLoginBtn"	//插入按钮的节点id
+
+});
+
+</script>
+
 </div>
 </body>
 </html>
