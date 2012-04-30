350c433d0bf09193969b507c860db27998cab40b
yaowenqiang <yaowenqiang111@163.com>
2012-04-30 13:09:01 +0800

    add the jquery library

--- back.php
+++ back.php
@@ -19,3 +19,19 @@ $params = array(	//构造请求参数
 */
 	var_dump($_GET);
 ?>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>认证通过，正在获取您的相关信息，请稍后 </title>
+</head>
+<body>
+	处理中，请稍等.
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript">
+	$(function() {
+		console.log('authed.');			
+	});
+</script>
+</body>
+</html>
