02e6a83d68212d51447bfe01fc8ffb64c40a56f3
yaowenqiang <yaowenqiang111@163.com>
2012-04-30 13:25:57 +0800

    fix the wrong grant_type

--- back.php
+++ back.php
@@ -2,7 +2,7 @@
 if (isset( $_GET['code']) && isset($_GET['state'])) {
 $remoteUrl = 'https://graph.qq.com/oauth2.0/token';		//远程请求地址 
 $params = array(	//构造请求参数
-	'grant_type' => 'code',
+	'grant_type' => 'authorization_code',
 	'client_id' => '100266584',
 	'client_secret' =>'2e499a2c58e20f1adb74c31d497a96b4',
 	'code' => $_GET['code'],
