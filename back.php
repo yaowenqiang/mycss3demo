3857b2b9b6c28fc15320feec1a56def6ccbefa24
yaowenqiang <yaowenqiang111@163.com>
2012-04-30 13:18:29 +0800

    remove the jquery library,and change the direct method to file method

--- back.php
+++ back.php
@@ -1,5 +1,4 @@
 <?php
-/*
 if (isset( $_GET['code']) && isset($_GET['state'])) {
 $remoteUrl = 'https://graph.qq.com/oauth2.0/token';		//远程请求地址 
 $params = array(	//构造请求参数
@@ -11,27 +10,12 @@ $params = array(	//构造请求参数
 	'redirect_uri' => 'http://mycss3demo.orchestra.io/back.php'
 );
 	$requestUrl = $remoteUrl.http_build_query($params);
-	header('location:'.'?'.$requestUrl);
+	$result = file($requestUrl);
+	var_dump($result);
+	// header('location:'.'?'.$requestUrl);
 	// ($remoteUrl);
 }else {
 	var_dump($_GET);
 }
-*/
-	var_dump($_GET);
+
 ?>
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>认证通过，正在获取您的相关信息，请稍后 </title>
-</head>
-<body>
-	处理中，请稍等.
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-<script type="text/javascript">
-	$(function() {
-		console.log('authed.');			
-	});
-</script>
-</body>
-</html>
