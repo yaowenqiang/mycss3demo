76ff220a498dbb21f5bbf53b449b50ed762af72f
yaowenqiang <yaowenqiang111@163.com>
2012-04-30 17:30:15 +0800

    close the error display

--- fsock.php
+++ fsock.php
@@ -1,8 +1,8 @@
 <?php
 	// if ($_SERVER[ 'POST' == 'REQUEST_METHOD']) {
 		// code...
-		// $remoteDir = 'https://graph.qq.com/t/add_t';
-		$remoteDir = 'google.com';
+		ini_set('display_errors','stderr');
+		$remoteDir = 'https://graph.qq.com/t/add_t';
 		$params=array(
 			'access_token' => $_POST['access_token'],
 			'oauth_consumer_key' => $_POST['oauth_consumer_key'],
@@ -11,21 +11,20 @@
 			'content' => $_POST['content']
 		);
 		$fp = fsockopen($remoteDir,80);
-		// $content = http_build_query($params);
-		// fwrite($fp, "POST /reposter.php HTTP/1.1\r\n");
+		$content = http_build_query($params);
+		fwrite($fp, "POST /reposter.php HTTP/1.1\r\n");
 		fwrite($fp, "Host: example.com\r\n");
 		fwrite($fp, "Content-Type: application/x-www-form-urlencoded\r\n");
 		fwrite($fp, "Content-Length: ".strlen($content)."\r\n");
 		fwrite($fp, "Connection: close\r\n");
 		fwrite($fp, "\r\n");
-		// fwrite($fp, $content);
+		fwrite($fp, $content);
 		header('Content-type: text/plain');
 		while (!feof($fp)) {
     		$result.= fgets($fp, 1024);
 		}
 		fclose($fp);
-		// echo $result;
-		print_r(stream_get_transports());
+		echo $result;
 		return;
 	//}
 ?>
