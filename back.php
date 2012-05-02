5416fe41d24f356ddc7bc261596e95d835e0c38c
yaowenqiang <yaowenqiang111@163.com>
2012-05-02 23:02:28 +0800

    add the push method

--- back.php
+++ back.php
@@ -145,8 +145,8 @@ function getfans(url)
 				fans = JSON.parse($(data).text());
 				$.each(fans.data.info,function(i,item){
 					if(item.city_code == userinfo.data.city_code){
-						$('<li>').html('姓名：'+item.nick).appendTo('#fanslist');
-						findfans[]  = item.nick;
+						// $('<li>').html('姓名：'+item.nick).appendTo('#fanslist');
+						findfans.push(item.nick);
 					}
 				});
 			}
